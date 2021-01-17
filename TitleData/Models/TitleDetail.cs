577998d1b739fe3c147469a8b9ed73362a7cbfbb
Chris <chris.albright@bluesombrero.com>
2021-01-16 19:39:47 -0500

    Add projects

---
+++ TitleData/Models/TitleDetail.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TitleData.Models
+{
+    public class TitleDetail
+    {
+        public List<Award> Award { get; set; }
+        public Genre Genre { get; set; }
+        public OtherName OtherName { get; set; }
+        public Participant Participant { get; set; }
+        public StoryLine StoryLine { get; set; }
+        public TitleGenre TitleGenre { get; set; }
+        public TitleParticipant TitleParticipant { get; set; }
+    }
+}
